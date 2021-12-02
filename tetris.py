e3fd797fad73083646592caf5099ae05de818fc3
Ondřej <ondra.saba@gmail.com>
2021-12-02 18:29:12 +0100

    added: brick movement

--- tetris.py
+++ tetris.py
@@ -25,6 +25,9 @@ class Brick:
         self.color = shapes.colors[self.type]
         (self.width, self.height) = shapes.dimensions[self.type]
 
+        # movement
+        self.locked = False
+
         # brick position
         self.x = 3 if self.width >= 3 else 4
         self.y = 0
@@ -42,10 +45,32 @@ class Brick:
         else:
             return []
 
+    def move(self, x, y):
+        # check if the brick isn't locked
+        if not self.locked:
+            # check if can move down
+            if self.y+self.height+y <= tiles_y:
+                self.y += y
+            else:
+                self.lock()
+
+            # check if can move to side
+            if self.x+self.width+x <= tiles_x and self.x+x >= 0:
+                self.x += x
+
+    def lock(self):
+        self.locked = True
+
+        for (tx, ty) in self.get_brick_positions():
+            game_map[self.x+tx][self.y+ty] = self.type
+
 brick = Brick()
 
 # game loop
+clock = pygame.time.Clock()
+rendered_frames = 0
 running = True
+
 while running:
     # dark background
     screen.fill((0, 0, 0))
@@ -54,13 +79,24 @@ while running:
     pygame.draw.rect(screen, (255, 255, 255), (300, 0, 2, screen_height))
 
     # draw the current brick
-    for brick_tile in brick.get_brick_positions():
-        (tx, ty) = brick_tile
-
+    for (tx, ty) in brick.get_brick_positions():
         pygame.draw.rect(screen, brick.color, ((brick.x + tx) * tile_size, (brick.y + ty) * tile_size, tile_size, tile_size))
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT:
+                brick.move(-1, 0)
+            elif event.key == pygame.K_RIGHT:
+                brick.move(1, 0)
+
+    rendered_frames += 1
+    if rendered_frames % 15 == 0:
+        brick.move(0, 1)
+
+        if brick.locked:
+            brick = Brick()
 
     pygame.display.update()
+    clock.tick(60)
